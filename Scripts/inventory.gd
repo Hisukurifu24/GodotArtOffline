ef0ce13eb46dce1558145374efc42a51e576b356
Francesco Naletto <46630982+Hisukurifu24@users.noreply.github.com>
2024-06-30 09:01:47 +0200

    inventory class component

--- Scripts/inventory.gd
+++ Scripts/inventory.gd
@@ -1,1 +1,19 @@
-class_name Inventory
+class_name InventoryComponent
+extends Node2D
+
+@export var player: Entity
+@export var items: Array[Item] = []
+
+# Add an item to the inventory
+func add_item(item: Item):
+    # Add the item to the inventory
+    items.append(item)
+    # Increase the player's stats
+    player.bonusStats.increase(item.stats)
+
+# Remove an item from the inventory
+func remove_item(item: Item):
+    # Remove the item from the inventory
+    items.erase(items.find(item))
+    # Decrease the player's stats
+    player.bonusStats.decrease(item.stats)
