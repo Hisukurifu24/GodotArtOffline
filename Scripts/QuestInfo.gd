cc010926e749edb20df12edb37187ef4d8b2ba91
Francesco Naletto <46630982+Hisukurifu24@users.noreply.github.com>
2024-07-19 13:23:51 +0200

    Update QuestInfo.gd

--- Scripts/QuestInfo.gd
+++ Scripts/QuestInfo.gd
@@ -1,8 +1,9 @@
 class_name QuestInfo
 extends Resource
 
+@export var id: int
 ## Nome della quest
-@export var name: String = ""
+@export_multiline var name: String = ""
 ## Descrizione della quest
 @export_multiline var description: String = ""
 ## Obiettivi della quest
@@ -10,7 +11,7 @@ extends Resource
 ## Ricompensa della quest
 @export var reward: Reward
 
-@export_category("Requisiti")
+@export_group("Requisiti")
 ## Livello minimo richiesto per accettare la quest
 @export var min_level: int = 1
 ## Quest richieste per accettare la quest
