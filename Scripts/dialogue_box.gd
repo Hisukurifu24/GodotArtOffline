c5393e4d6b71bfd6cec24f856cf1eca040b51f0c
Francesco Naletto <f.naletto@outlook.com>
2024-07-22 00:35:51 +0200

    Implementate le funzionalità base del Dialogo

--- Scripts/dialogue_box.gd
+++ Scripts/dialogue_box.gd
@@ -1,3 +1,4 @@
+class_name DialogueBox
 extends CanvasLayer
 
 ## Linee di dialogo
@@ -6,28 +7,85 @@ extends CanvasLayer
 var currentDialog = null
 var currentIndex: int = 0
 
-@onready var textLabel: Label = $"Control/Control/Label"
+@onready var textLabel: Label = $"Control/Content/Text"
+@onready var answers: CanvasItem = $"Answers"
+@onready var option1: Button = $"Answers/Option1"
+@onready var option2: Button = $"Answers/Option2"
+@onready var option3: Button = $"Answers/Option3"
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	loadDialogData()
+
+	# Connect the pressed signal of the buttons to the corresponding functions
+	option1.pressed.connect(_on_Option1_pressed)
+	option2.pressed.connect(_on_Option2_pressed)
+	option3.pressed.connect(_on_Option3_pressed)
+
 	# Set the icon as the sprite of the NPC
 	$"Control/TextureRect".texture = $"../Sprite2D".texture
-	pass
+	# Set the name of the NPC
+	$"Control/Content/Name".text = currentDialog["Name"]
+	# Set the first line of the dialog
+	updateDialog()
 
-	textLabel.text = currentDialog["Dialogs"][currentIndex]["Text"]
-
-# func _input(event):
-# 	if event.is_action_pressed("ui_accept"):
-# 		print(str(lines.size()) + " - " + str(currentIndex))
-# 		if currentIndex < lines.size() - 1:
-# 			currentIndex += 1
-# 			$"Control/Control/Label".text = lines[currentIndex]
-# 		else:
-# 			# chiudi label e fai cose
-# 			pass
+func _input(event):
+	if event.is_action_pressed("ui_accept"):
+		print("Accept")
+		# Avanza il dialogo
+		updateDialog()
+		pass
 
 func loadDialogData():
 	var file = FileAccess.open(dialogPath, FileAccess.READ)
 	currentDialog = JSON.parse_string(file.get_as_text())
 	# print(currentDialog["Dialogs"][currentIndex]["Text"])
+
+func updateDialog():
+	print(currentIndex)
+	# Se il dialogo è finito
+	if currentIndex == - 1:
+		visible = false
+		currentIndex = 0
+		return
+	# Se il dialogo ha delle opzioni
+	if currentDialog["Dialogs"][currentIndex].has("Options"):
+		# mostra il testo
+		textLabel.text = currentDialog["Dialogs"][currentIndex]["Text"]
+		# mostra opzioni
+		answers.visible = true
+
+		# (default) mostra opzione 1 e nascondi le altre
+		answers.get_node("Option1").visible = true
+		answers.get_node("Option2").visible = false
+		answers.get_node("Option3").visible = false
+
+		# mostra testo dell'opzione 1
+		answers.get_node("Option1/Label").text = currentDialog["Dialogs"][currentIndex]["Options"][0]["Text"]
+
+		# se ci sono altre opzioni, mostrale
+		if currentDialog["Dialogs"][currentIndex]["Options"].size() > 1:
+			answers.get_node("Option2").visible = true
+			answers.get_node("Option2/Label").text = currentDialog["Dialogs"][currentIndex]["Options"][1]["Text"]
+		if currentDialog["Dialogs"][currentIndex]["Options"].size() > 2:
+			answers.get_node("Option3").visible = true
+			answers.get_node("Option3/Label").text = currentDialog["Dialogs"][currentIndex]["Options"][2]["Text"]
+	else:
+		answers.visible = false
+		
+		# mostra il testo
+		textLabel.text = currentDialog["Dialogs"][currentIndex]["Text"]
+		# muovi l'indice
+		currentIndex = currentDialog["Dialogs"][currentIndex]["NextDialogId"] - 1
+
+func _on_Option1_pressed():
+	currentIndex = currentDialog["Dialogs"][currentIndex]["Options"][0]["NextDialogId"] - 1
+	updateDialog()
+
+func _on_Option2_pressed():
+	currentIndex = currentDialog["Dialogs"][currentIndex]["Options"][1]["NextDialogId"] - 1
+	updateDialog()
+
+func _on_Option3_pressed():
+	currentIndex = currentDialog["Dialogs"][currentIndex]["Options"][2]["NextDialogId"] - 1
+	updateDialog()
--- Scripts/npc.gd
+++ Scripts/npc.gd
@@ -1,13 +1,26 @@
 extends CharacterBody2D
 
+var interactable := false
 
-func _on_area_2d_body_entered(body:Node2D):
-	if(body.name == "Character"):
+func _input(event):
+	if event.is_action_pressed("ui_accept") and interactable:
+		# Show the dialogue box
 		$DialogueBox.process_mode = Node.PROCESS_MODE_INHERIT
 		$DialogueBox.show()
+		$DialogueBox.updateDialog()
 
+func _on_area_2d_body_entered(body: Node2D):
+	if (body.name == "Character"):
+		interactable = true
 
-func _on_area_2d_body_exited(body:Node2D):
-	if(body.name == "Character"):
+func _on_area_2d_body_exited(body: Node2D):
+	if (body.name == "Character"):
+		interactable = false
+
+		# Hide the dialogue box
 		$DialogueBox.process_mode = Node.PROCESS_MODE_DISABLED
 		$DialogueBox.hide()
+
+		# Reset the dialogue box
+		$DialogueBox.currentIndex = 0
+		$DialogueBox.updateDialog()
