d9a607901da9bf6b7f0b3694a9863e75e83a7c9e
Francesco Naletto <f.naletto@outlook.com>
2024-08-20 01:13:06 +0200

    New functions for inventory
    Inventory now has a size (6 for inv, 50 for bag)
    add and remove func now checks for full or not found
    new insert_at and remove_at

--- Scripts/inventory.gd
+++ Scripts/inventory.gd
@@ -4,15 +4,23 @@ extends Node2D
 signal item_collected(item: Item)
 
 @export var player: Entity
-@export var items: Array[Item] = []
+@export var inventorySize: int = 6
+@export var _items: Array[Item] = []
+
+func _ready():
+    _items.resize(inventorySize)
 
 ## Whether the inventory is active or passive (items increase player stats or not)
 @export var isActiveInventory: bool = true
 
-# Add an item to the inventory
+## Add an item to the inventory
 func add_item(item: Item):
     # Add the item to the inventory
-    items.append(item)
+    var index = _items.find(null)
+    if index == -1:
+        printerr("Inventory is full")
+        return
+    _items[index] = item
     
     item_collected.emit(item)
     Quest_Manager.item_collected.emit(item) # Emit signal for quest system
@@ -21,11 +29,40 @@ func add_item(item: Item):
         # Increase the player's stats
         player.bonusStats.increase(item.stats)
 
-# Remove an item from the inventory
+func insert_at(item: Item, index: int):
+    if index < 0 or index >= _items.size():
+        printerr("Invalid index")
+        return
+    print("Inserting" + item.name + "at index " + str(index))
+    _items[index] = item
+
+## Remove an item from the inventory
 func remove_item(item: Item):
     # Remove the item from the inventory
-    items.erase(items.find(item))
+    var index = _items.find(item)
+    if index == -1:
+        printerr("Item not found in inventory")
+        return
+    _items[index] = null
     
     if isActiveInventory:
         # Decrease the player's stats
         player.bonusStats.decrease(item.stats)
+
+func remove_at(index: int):
+    if index < 0 or index >= _items.size():
+        printerr("Invalid index")
+        return
+    print("Removing item at index " + str(index))
+    _items[index] = null
+
+## Get the item at the specified index
+func get_item(index: int) -> Item:
+    if index < 0 or index >= _items.size():
+        return null
+    return _items[index]
+
+func print_inventory():
+    print(name + ":")
+    for i in range(_items.size()):
+        print(_items[i])
